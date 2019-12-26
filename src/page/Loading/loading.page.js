b0aef3ad6cfc61fd89cd0792e78a57e2565e558b
Luong Minh Tam <tam.luong-minh@internal.banvien.com>
2019-12-26 09:13:07 +0700

    Fix login

--- src/page/Loading/loading.page.js
+++ src/page/Loading/loading.page.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {View} from 'react-native';
 import Color from '../../shared/Color';
 import firebase from 'firebase';
+import Ticket from '../../../firebaseConfig';
 
 export default class LoadingPage extends React.Component {
   static navigationOptions = {
@@ -10,11 +11,22 @@ export default class LoadingPage extends React.Component {
   };
 
   componentDidMount() {
-    firebase
-      .auth()
-      .onAuthStateChanged(user =>
-        this.props.navigation.navigate(user ? 'Profile' : 'Login'),
-      );
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        Ticket.database()
+          .ref()
+          .child('users')
+          .once('value', snapshot => {
+            if (snapshot.hasChild(firebase.auth().currentUser.uid)) {
+              this.props.navigation.navigate('Profile');
+            } else {
+              this.props.navigation.navigate('SignUp', {
+                isNotHaveAccount: true,
+              });
+            }
+          });
+      } else this.props.navigation.navigate('Login');
+    });
   }
 
   render() {
